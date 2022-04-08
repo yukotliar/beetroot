3acc289a0a990fdf0c94d63b620a7b63e5021905
yurii.kotliar <yukotliar@gmail.com>
2022-04-08 17:03:26 +0300

    small fixes

--- lesson1/task2.py
+++ lesson1/task2.py
@@ -1,4 +1,4 @@
 print('lorem', 'ipsum', 'lorem', 'ipsum', end='\n')
 # this prints several strings with default sep and '\n' end parameter (new line)
 print('lorem', 'ipsum', 'lorem', 'ipsum', sep='_')
-# this prints several string with default end and '_' sep parameter
+# this prints several strings with default end and '_' sep parameter
