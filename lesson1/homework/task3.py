db43d27520aeedca2a766b63f10f314189bc9337
yurii.kotliar <yukotliar@gmail.com>
2022-04-09 11:33:27 +0300

    small fixes

---
+++ lesson1/homework/task3.py
@@ -0,0 +1,2 @@
+print('#' * 9, '#\t\t#', '#\t\t#', '#\t\t#', '#' * 9, sep='\n')  # 'O' printed with '#' symbols
+print('\n#\t\t#', '#\t\t#', '#' * 9, '#\t\t#', '#\t\t#', sep='\n')  # 'H' printed with '#' symbols
