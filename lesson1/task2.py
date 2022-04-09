1b2212b758f87a38023ce69a1e7c5efc6fcba2b7
yurii.kotliar <yukotliar@gmail.com>
2022-04-09 11:31:23 +0300

    small fixes

--- lesson1/task2.py
+++
@@ -1,4 +0,0 @@
-print('lorem', 'ipsum', 'lorem', 'ipsum', end='\n')
-# this prints several strings with default sep and '\n' end parameter (new line)
-print('lorem', 'ipsum', 'lorem', 'ipsum', sep='_')
-# this prints several strings with default end and '_' sep parameter
--- lesson1/task3.py
+++
@@ -1,2 +0,0 @@
-print('#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n')  # 'O' printed with '#' symbols
-print('#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#')  # 'H' printed with '#' symbols
