738279ee67bc5befb138b039f8061f304750abf3
yurii.kotliar <yukotliar@gmail.com>
2022-04-10 17:47:35 +0300

    lesson 3 homework done

---
+++ lesson3/homework/task3.py
@@ -0,0 +1,11 @@
+# The name check.
+# Write a program that has a variable with your name stored (in lowercase)
+# and then asks for your name as input. The program should check if your input
+# is equal to the stored name even if the given name has another case, e.g.,
+# if your input is “Anton” and the stored name is “anton”, it should return True.
+stored_name = 'yurii'
+entered_name = input('Enter your name: ')
+if entered_name.lower() == stored_name:
+    print(f'I recognize you, {stored_name}!')
+else:
+    print('I don\'t recognize you, scammer!')
