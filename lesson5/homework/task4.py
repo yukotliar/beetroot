98e440557a0ac5bde35d57a672a03411d2ef2923
yurii.kotliar <yukotliar@gmail.com>
2022-04-15 21:03:21 +0300

    lesson 5 homework completed

--- lesson5/homework/task4.py
+++ lesson5/homework/task4.py
@@ -2,3 +2,14 @@
 #
 # Write a program that asks the answer for a mathematical expression,
 # checks whether the user is right or wrong, and then responds with a message accordingly.
+from lesson5.work_with_input import *
+
+import math
+
+print('Hello, human! This program is made to test your factorial calculation skills.')
+value = input_integer('Enter any int value: ')
+user_answer = input_integer('Enter its factorial: ')
+if math.factorial(value) == user_answer:
+    print('That\'s correct. You\'re pure genius!')
+else:
+    print('Sorry, your answer is wrong.')
