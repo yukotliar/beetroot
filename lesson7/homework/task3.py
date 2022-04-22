0c2c270791dc95428c91c5093bf6fde8149295ac
yurii.kotliar <yukotliar@gmail.com>
2022-04-22 11:30:02 +0300

    lesson 7 homework completed

--- lesson7/homework/task3.py
+++ lesson7/homework/task3.py
@@ -4,5 +4,6 @@
 #
 # Use a list comprehension to make a list containing tuples (i, j)
 # where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
-tuples_list = [[] ]
+
+tuples_list = [(i, i**2)for i in range(1, 11)]
 print(tuples_list)
