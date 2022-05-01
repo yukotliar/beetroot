4d89c8bd675e19549402ce4de56417ec6d03169c
yurii.kotliar <yukotliar@gmail.com>
2022-05-02 00:29:21 +0300

    lesson 13 homework task 3

---
+++ lesson13/Homework/task3.py
@@ -0,0 +1,32 @@
+# Write a function called `choose_func` which takes a list of nums and 2 callback functions.
+# If all nums inside the list are positive, execute the first function on that list and return the result of it.
+# Otherwise, return the result of the second one
+
+def choose_func(nums: list, func1, func2):
+    if sum(value < 0 for value in nums):
+        return func2(nums)
+    else:
+        return func1(nums)
+
+
+# Assertions
+
+nums1 = [1, 2, 3, 4, 5]
+
+nums2 = [1, -2, 3, -4, 5]
+
+
+def square_nums(nums):
+    return [num ** 2 for num in nums]
+
+
+def remove_negatives(nums):
+    return [num for num in nums if num > 0]
+
+
+assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
+
+assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
+
+print(choose_func(nums1, square_nums, remove_negatives))
+print(choose_func(nums2, square_nums, remove_negatives))
