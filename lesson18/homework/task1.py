443ca19461e0ed1e1aabdb96410a17ed4eb177ff
yurii.kotliar <yukotliar@gmail.com>
2022-05-15 22:31:13 +0300

    lesson 18 homework task 1 completed

---
+++ lesson18/homework/task1.py
@@ -0,0 +1,19 @@
+# Create a class method named `validate`, which should be called from the `__init__` method to validate parameter email,
+# passed to the constructor. The logic inside the `validate` method could be to check if the passed email parameter
+# is a valid email string.
+
+import re
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+
+class Email:
+    def __init__(self, email):
+        if (re.fullmatch(regex, email)):
+            self.email = email
+            print("valid email")
+        else:
+            raise ValueError('entered email is invalid')
+
+
+ron = Email('ronald@gmail.com')
