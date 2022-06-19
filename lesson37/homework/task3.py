ab54d5fa7b69d33027f9bef512d22166f605399d
yurii.kotliar <yukotliar@gmail.com>
2022-06-19 17:22:29 +0300

    lesson 37 homework

---
+++ lesson37/homework/task3.py
@@ -0,0 +1,14 @@
+# Write a console application which takes as an input a city name and returns current weather in the format of your choice.
+# For the current task, you can choose any weather API or website or use openweathermap.org
+import requests
+
+base_url = "http://api.weatherapi.com/v1/current.json"
+params = dict()
+params["key"] = "bac8006360b7473eba0134824221906"
+params["q"] = "London"
+params['aqi'] = "no"
+
+r = requests.get(url=base_url, params=params)
+data = r.json()
+print(
+    f'{data["location"]["name"]}, {data["location"]["country"]}, {data["current"]["temp_c"]} °C, {data["current"]["condition"]["text"]}')
