40c5228ef7d839a52793adbc550d30e9b2408718
yurii.kotliar <yukotliar@gmail.com>
2022-06-22 17:57:43 +0300

    lesson 38 homework

--- lesson38/homework/task1.py
+++ lesson38/homework/task1.py
@@ -9,5 +9,7 @@ results = soup.find("div", class_="card bg-white m-0")
 job_elements = results.find_all("div", class_="card-body px-3 py-0 pl-md-0")
 # print(results)
 for job_element in job_elements:
-    title_element = job_element.find("p", class_="m-0")
-    print(title_element.text)
+    title_element = job_element.find("span", class_="font-weight-bold larger")
+    title_element = title_element.text.strip()
+    if "Senior" and "React" in title_element:
+        print(title_element)
